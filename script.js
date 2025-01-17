210e80dee87937dff21e72f6989fb8d7ef7715b5
mchlrc <108034624+mchlrc@users.noreply.github.com>
2025-01-16 19:02:54 -0700

    Add Functionality to Get The Human's Choice
    Added a function to prompt the user for input.

--- script.js
+++ script.js
@@ -4,3 +4,29 @@ function getComputerChoice() {
   let choice = Math.floor(Math.random() * 3);
   return choices[choice];
 }
+
+// Get the human's choice
+function getHumanChoice() {
+    let choices = ["Rock", "Paper", "Scissors"];
+    let choiceIsValid = false;
+    let choice = "";
+    let basePrompt = "Choose Rock, Paper, or Scissors:";
+    let thePrompt = basePrompt;
+
+    while (!choiceIsValid) {
+        choice = prompt(thePrompt);
+
+        if (choice === null) {
+            choice = getComputerChoice();
+        } else {
+            choice = choice.toLowerCase().charAt(0).toUpperCase() + choice.slice(1)
+        }
+
+        if (choices.includes(choice)) {
+            choiceIsValid = true;
+        } else {
+            thePrompt = `${choice} is not a valid choice.\n${basePrompt}`;
+        }
+    }
+    return choice;
+}
